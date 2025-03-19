cd63fac7219fc4160c41c95b9643cc8bb89d8448
WendyFyfo <wendyfyfo@gmail.com>
2025-03-19 11:23:50 +0700

    [GREEN] implement PaymentService

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentService.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface PaymentService {
-    Payment addPayment(String paymentId, Order order, String method, Map<String, String> paymentData);
+    Payment addPayment(Order order, String method, Map<String, String> paymentData);
     Payment setStatus(Payment payment, String status);
     Payment getPayment(String paymentId);
     List<Payment> getAllPayments();
--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -1,11 +1,14 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
+import enums.OrderStatus;
+import enums.PaymentMethod;
+import enums.PaymentStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
-
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 public class PaymentServiceImpl implements PaymentService {
     private final PaymentRepository paymentRepository;
@@ -15,22 +18,39 @@ public class PaymentServiceImpl implements PaymentService {
     }
 
     @Override
-    public Payment addPayment(String paymentId, Order order, String method, Map<String, String> paymentData) {
-        return null;
+    public Payment addPayment(Order order, String method, Map<String, String> paymentData) {
+        if (!PaymentMethod.contains(method)) {
+            throw new IllegalArgumentException("Invalid payment method");
+        }
+        Payment payment = new Payment(order.getId(), method, paymentData);
+        return paymentRepository.save(payment, order);
     }
 
     @Override
     public Payment setStatus(Payment payment, String status) {
-        return null;
+        if (!PaymentStatus.contains(status)) {
+            throw new IllegalArgumentException("Invalid payment status");
+        }
+        Order order = paymentRepository.getOrder(payment.getId());
+        if (order != null) {
+            if (Objects.equals(status, PaymentStatus.SUCCESS.name())) {
+                order.setStatus(OrderStatus.SUCCESS.getValue());
+            } else if (Objects.equals(status, PaymentStatus.REJECTED.name())) {
+                order.setStatus(OrderStatus.FAILED.getValue());
+            }
+        }
+        paymentRepository.update(payment, status);
+        payment.setStatus(status);
+        return payment;
     }
 
     @Override
     public Payment getPayment(String paymentId) {
-        return null;
+        return paymentRepository.findById(paymentId);
     }
 
     @Override
     public List<Payment> getAllPayments() {
-        return null;
+        return paymentRepository.findAll();
     }
 }
