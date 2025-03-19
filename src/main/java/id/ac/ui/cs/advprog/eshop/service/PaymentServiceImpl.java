684e6a24849e3857c0005b94e8eace2a9d911dd0
WendyFyfo <wendyfyfo@gmail.com>
2025-03-19 11:06:29 +0700

    [RED] Create skeleton for PaymentService interface and PaymentServiceImpl

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/PaymentServiceImpl.java
@@ -1,2 +1,36 @@
-package id.ac.ui.cs.advprog.eshop.service;public class PaymentServiceImpl {
+package id.ac.ui.cs.advprog.eshop.service;
+
+import id.ac.ui.cs.advprog.eshop.model.Order;
+import id.ac.ui.cs.advprog.eshop.model.Payment;
+import id.ac.ui.cs.advprog.eshop.repository.PaymentRepository;
+
+import java.util.List;
+import java.util.Map;
+
+public class PaymentServiceImpl implements PaymentService {
+    private final PaymentRepository paymentRepository;
+
+    public PaymentServiceImpl(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
+    }
+
+    @Override
+    public Payment addPayment(String paymentId, Order order, String method, Map<String, String> paymentData) {
+        return null;
+    }
+
+    @Override
+    public Payment setStatus(Payment payment, String status) {
+        return null;
+    }
+
+    @Override
+    public Payment getPayment(String paymentId) {
+        return null;
+    }
+
+    @Override
+    public List<Payment> getAllPayments() {
+        return null;
+    }
 }
