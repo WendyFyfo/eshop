eb11c8a595c93c3c7a889f9f5849ec764ba1a5b0
WendyFyfo <wendyfyfo@gmail.com>
2025-03-19 10:41:31 +0700

    [GREEN] Implement PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,29 +1,71 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import enums.OrderStatus;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
-import lombok.Builder;
-import lombok.Getter;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Repository
 public class PaymentRepository {
+    private Map<String, Payment> paymentData = new HashMap<>();
+    private Map<String, Order> paymentOrder = new HashMap<>();
+
     public Payment save(Payment payment, Order order) {
-        return null;
+        if (payment == null || order == null) {
+            throw new IllegalArgumentException("Payment or order cannot be null");
+        }
+        if (paymentData.containsKey(payment.getId())) {
+            throw new IllegalArgumentException("Payment with this ID already exists");
+        }
+
+        paymentData.put(payment.getId(), payment);
+        paymentOrder.put(payment.getId(), order);
+
+        if ("SUCCESS".equals(payment.getStatus())) {
+            order.setStatus(OrderStatus.SUCCESS.getValue());
+        } else {
+            order.setStatus(OrderStatus.FAILED.getValue());
+        }
+
+        return payment;
     }
 
     public void update(Payment payment, String status) {
+        if (payment == null || status == null) {
+            throw new IllegalArgumentException("Payment or status cannot be null");
+        }
+        if (!paymentData.containsKey(payment.getId())) {
+            throw new IllegalArgumentException("Payment not found");
+        }
+
+        payment.setStatus(status);
+        Order order = paymentOrder.get(payment.getId());
+
+        if ("SUCCESS".equals(status)) {
+            order.setStatus(OrderStatus.SUCCESS.getValue());
+        } else {
+            order.setStatus(OrderStatus.FAILED.getValue());
+        }
     }
 
-    public Payment findById(String id) {
-        return null;
+    public Payment findById(String paymentId) {
+        if (paymentId == null) {
+            throw new NullPointerException("Payment ID cannot be null");
+        }
+        return paymentData.get(paymentId);
     }
 
     public List<Payment> findAll() {
-        return null;
+        return new ArrayList<>(paymentData.values());
     }
 
-    public Order getOrder(String id) {
-        return null;
+    public Order getOrder(String paymentId) {
+        return paymentOrder.get(paymentId);
     }
 }
+
